1e405565fd07db2bc48c8ada1b05446babc2947b
Mhattosx <88255859+Mhattosx@users.noreply.github.com>
2021-08-11 20:20:53 -0300

    update playercontroller
    add function for control movement and rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _speedRotate = 4f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovementHandler();
+        RotateHandler();
+    }
+
+    private void MovementHandler()
+    {
+        var position = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (position != Vector3.zero)
+        {
+            transform.Translate(position * (_speed * Time.deltaTime));
+        }
+    }
+
+    private void RotateHandler()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X")* _speedRotate, 0f));
+        }
     }
 }
