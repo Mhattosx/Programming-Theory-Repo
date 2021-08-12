548d4310a49a2057e40461bb02e60f84b3efccab
Mhattosx <88255859+Mhattosx@users.noreply.github.com>
2021-08-11 22:50:45 -0300

    create class Animal

--- Assets/Scripts/Managers/Animals/FoxController.cs
+++ Assets/Scripts/Managers/Animals/FoxController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoxController : MonoBehaviour
+public class FoxController : Animal
 {
     // Start is called before the first frame update
     void Start()
